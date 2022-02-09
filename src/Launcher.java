19eeb5ea30de6152cc5ca72efb6750e2ff34cd04
Clarel Salassa <clarelsalassapro@gmail.com>
2022-02-09 21:59:20 +0100

    Partie 4

--- src/Launcher.java
+++ src/Launcher.java
@@ -6,9 +6,33 @@ public class Launcher {
         Scanner scanner = new Scanner(System.in);
         while (true) {
             String input = scanner.nextLine();
-            if (!input.equals("quit")) {
+            if (input.equals("fibo"))
+            {
+                System.out.println("Entrez un nombre pour l'index de la suite de " +
+                        "fibonacci:");
+                int value = Integer.valueOf(scanner.nextLine());
+                int f0 = 0;
+                int f1 = 1;
+                int ff;
+                for (int i = 1; i < value; i++){
+                    ff = f1;
+                    f1 = f0 + f1;
+                    f0 = ff;
+                }
+                if (value == 0)
+                {
+                    System.out.println("F(" + value + ") = " + 0);
+                }
+                else
+                {
+                    System.out.println("F(" + value + ") = " + f1);
+                }
+
+            }
+            else if (!input.equals("quit")) {
                 System.out.println("Unknown command");
             }
+
             else
             {
                 break;
