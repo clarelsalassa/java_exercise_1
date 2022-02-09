e13cdbb9caec901b2255ebfd4a110bfa141ec61b
Clarel Salassa <clarelsalassapro@gmail.com>
2022-02-09 21:41:29 +0100

    Partie 3

--- src/Launcher.java
+++ src/Launcher.java
@@ -3,11 +3,16 @@ import java.util.Scanner;
 public class Launcher {
     public static void main(String[] args) {
         System.out.println("Bienvenue dans la faille de l'invocateur");
-        Scanner input = new Scanner(System.in);
-        String s = input.nextLine();
-        if (!s.equals("quit"))
-        {
-            System.out.println("Unknown command");
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            if (!input.equals("quit")) {
+                System.out.println("Unknown command");
+            }
+            else
+            {
+                break;
+            }
         }
     }
 }
