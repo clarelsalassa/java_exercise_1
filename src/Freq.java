dd0856e909e5e8d88706f0c81194f5703994a136
Clarel Salassa <clarelsalassapro@gmail.com>
2022-02-10 22:07:17 +0100

    Partie 6 v2

--- src/Freq.java
+++ src/Freq.java
@@ -2,8 +2,8 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Freq implements Command{
     @Override
@@ -17,43 +17,24 @@ public class Freq implements Command{
         String input = scanner.nextLine();
         Path path = Paths.get(input);
         try {
-            String content = Files.readString(path);
-            content.replaceAll("[^a-zA-Z0-9]+", " ").toLowerCase();
-            String[] array = content.split(" ");
-            ArrayList<String> res = new ArrayList<String>();
-            int x = 3;
-            while (x > 0) {
-                int maxCount = 0;
-                int count = 0;
-                String word = "";
-                for (int i = 0; i < array.length; i++) {
-                    if (!res.contains(array[i])){
-                        count = 1;
-                        for (int j = i + 1; j < array.length; j++){
-                            if (array[i].equals(array[j])){
-                                count++;
-                            }
-                        }
-                        if (count > maxCount){
-                            maxCount = count;
-                            word = array[i];
-                        }
-                    }
-
-                }
-                res.add(word);
-                x -= 1;
-            }
-            for (int y = 0; y < res.size(); y++){
-                if (y != res.size() - 1) {
-                    System.out.print(res.get(y) + " ");
+            String fileContent = Files.readString(path);
+            String[] tableau = fileContent.replaceAll("[^a-zA-Z0-9]+", " ").toLowerCase()
+                    .split(" ");
+            var res = Arrays.stream(tableau).
+                    collect(Collectors.groupingBy(e->e, Collectors.counting()));
+            var res2 = res.entrySet().stream().
+                    sorted(Comparator.comparing(Map.Entry<String,Long>::getValue).reversed()).limit(3);
+            var res3 = res2.collect(Collectors.toList());
+            for (int i = 0; i < res3.size(); i++){
+                if (i == res3.size() - 1){
+                    System.out.printf(res3.get(i).getKey().toString());
                 }
                 else {
-                    System.out.print(res.get(y));
+                    System.out.printf(res3.get(i).getKey().toString() + ' ');
                 }
+
             }
             System.out.println();
-            //System.out.println("Les 3 mots les plus fréquents sont: " + res.toString());
 
         }
         catch (IOException e) {
@@ -62,3 +43,40 @@ public class Freq implements Command{
         return false;
     }
 }
+/*
+content.replaceAll("[^a-zA-Z0-9]+", " ").toLowerCase();
+        String[] array = content.split(" ");
+        ArrayList<String> res = new ArrayList<String>();
+        int x = 3;
+        while (x > 0) {
+        int maxCount = 0;
+        int count = 0;
+        String word = "";
+        for (int i = 0; i < array.length; i++) {
+        if (!res.contains(array[i])){
+        count = 1;
+        for (int j = i + 1; j < array.length; j++){
+        if (array[i].equals(array[j])){
+        count++;
+        }
+        }
+        if (count > maxCount){
+        maxCount = count;
+        word = array[i];
+        }
+        }
+
+        }
+        res.add(word);
+        x -= 1;
+        }
+        for (int y = 0; y < res.size(); y++){
+        if (y != res.size() - 1) {
+        System.out.print(res.get(y) + " ");
+        }
+        else {
+        System.out.print(res.get(y));
+        }
+        }
+        System.out.println(); */
+//System.out.println("Les 3 mots les plus fréquents sont: " + res.toString());
--- src/Quit.java
+++ src/Quit.java
@@ -8,6 +8,7 @@ public class Quit implements Command {
 
     @Override
     public boolean run(Scanner scanner) {
+        System.out.println("bye bye");
         return true;
     }
 }
